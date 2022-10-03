d9009cf1f2ee8c5c683430c92b6a92137e8cb0e6
Eli Pagan <mbk8689@gmail.com>
2022-10-03 11:52:15 -0400

    GET photos through our getPhotos function

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,4 +1,20 @@
+
+
 // Unsplash API
-const count = 10;
-const apiKey = '';
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY&count=`
+const count = 15;
+const apiKey = 'PfAKg3uROx6GtFDW4HJ06y6-aZA1j-jMhkSWCMQeDBI';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error
+    }
+}
+
+// On Load
+getPhotos();
