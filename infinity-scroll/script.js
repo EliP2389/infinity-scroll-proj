b40da1ad27550e09b882d5776961f85c8714f77f
Eli Pagan <mbk8689@gmail.com>
2022-10-04 12:14:28 -0400

    fixed type error

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -16,13 +16,17 @@ displayPhotos = () => {
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+    });
     // Create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+    });
     // Put <img> inside <a>, then put both inside imgContainer Element
     item.appendChild(img);
     imgContainer.appendChild(item);
@@ -39,11 +43,11 @@ async function getPhotos() {
   try {
     const response = await fetch(apiURL);
     photosArray = await response.json();
-    displayPhotos();
+    displayPhotos()
   } catch (error) {
     // Catch Error
   }
 }
-console.log();
+
 // On Load
 getPhotos();
